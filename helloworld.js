fe3ddee867bfc0201bca637576da9633ae0438ee
Maurizio Tallarico <maurizio.tallarico@gmail.com>
2016-06-04 19:56:37 +0200

    Add files via upload

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,13 @@
 
 var clc= require('cli-color');
 
+var contatore;
 console.log('Hello World, nodejs');
 
 console.log(clc.red('"Ciao Mondo" in rosso'));
 console.log(clc.yellow('agragar'));
+for (contatore=0; contatore <100 ; contatore++){
+	/*console.log('il contatore ora è');
+	console.log(contatore);*/
+	console.log('il contatore ora è ' + contatore);
+}
